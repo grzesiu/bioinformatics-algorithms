45e27151701a30f04898f4e6cadaa001a1b0d2be
grzesiu <grzspychala@gmail.com>
2017-10-26 16:14:00 +0200

    Solve suffix tree construction

---
+++ 9_how_do_we_locate_disease_causing_mutations/suffix_tree_construction.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+
+    def add_child(self, label, child):
+        self.children[label] = child
+
+
+def create_suffix_trie(text):
+    root = Node()
+    for j in range(len(text)):
+        pattern = text[j:]
+        curr = root
+        for i in pattern:
+            if i in curr.children:
+                curr = curr.children[i]
+            else:
+                new = Node()
+                curr.add_child(i, new)
+                curr = new
+    return root
+
+
+def in_order(curr, edge):
+    if not len(curr.children):
+        print(edge)
+    elif len(curr.children) == 1:
+        label, child = next(iter(curr.children.items()))
+        in_order(child, edge + label)
+    else:
+        if edge:
+            print(edge)
+        for label, child in curr.children.items():
+            in_order(child, label)
+
+
+def main(text):
+    root = create_suffix_trie(text)
+    in_order(root, '')
+
+
+if __name__ == "__main__":
+    main("ATAAATG$")
+
+'''
+Given: A string Text.
+Return: The strings labeling the edges of SuffixTree(Text). (You may return these strings in any order.)
+Output:
+T
+G$
+AAATG$
+A
+T
+G$
+AAATG$
+A
+TG$
+ATG$
+G$
+$
+'''
